d639ab7daffd8a32297d86808f422ee6242b20ca
terziev-viktor <terzievviktor@gmail.com>
2017-10-09 17:15:39 +0300

    Compiled for any CPU

--- AESTest2.0/AESTest2.0/Form1.cs
+++ AESTest2.0/AESTest2.0/Form1.cs
@@ -111,7 +111,7 @@ namespace AESTest2._0
                 System.Windows.Forms.Application.Exit();
             }
             // Kills explorer.exe
-            //ExplorerManager.Kill();
+            ExplorerManager.Kill();
             this.EnterStage_1();
         }
 
